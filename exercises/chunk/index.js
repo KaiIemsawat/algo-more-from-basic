e5c884cfec7c4eb563e97800f0d3bc69d0f4b876
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-26 22:00:10 -0400

    more solutions

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,21 +9,38 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    let resultArr = [];
-    let tempArr = [];
-    if (array.length <= size) {
-        resultArr.push(array);
-        return resultArr;
-    }
+    const resultArr = [];
 
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i]);
-        tempArr.push(array[i]);
-        if (tempArr.length === size) {
-            resultArr.push(tempArr);
-            tempArr = [];
-        } else if (i === array.length - 1) {
-            resultArr.push(tempArr);
+    /* SOLUTION 1 */
+    // let tempArr = [];
+    // if (array.length <= size) {
+    //     return [array];
+    // }
+    // for (let i = 0; i < array.length; i++) {
+    //     tempArr.push(array[i]);
+    //     if (tempArr.length === size) {
+    //         resultArr.push(tempArr);
+    //         tempArr = [];
+    //     } else if (i === array.length - 1) {
+    //         resultArr.push(tempArr);
+    //     }
+    // }
+
+    /* SOLUTION 2 */
+    // if (array.length <= size) return [array];
+
+    // while (array.length > 0) {
+    //     resultArr.push(array.splice(0, size));
+    // }
+
+    /* SOLUTION 3 */
+    for (let element of array) {
+        const last = resultArr[resultArr.length - 1]; // for the first iteration, last will be undefined
+
+        if (!last || last.length === size) {
+            resultArr.push([element]); // push the new sub array along with the first element for that sub array
+        } else {
+            last.push(element);
         }
     }
 
