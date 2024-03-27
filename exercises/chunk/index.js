e02fd9ed259306a81c2de928a95912b26c755209
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-03-26 21:26:22 -0400

    chunk -self

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,28 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let resultArr = [];
+    let tempArr = [];
+    if (array.length <= size) {
+        resultArr.push(array);
+        return resultArr;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+        tempArr.push(array[i]);
+        if (tempArr.length === size) {
+            resultArr.push(tempArr);
+            tempArr = [];
+        } else if (i === array.length - 1) {
+            resultArr.push(tempArr);
+        }
+    }
+
+    return resultArr;
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5));
 
 module.exports = chunk;
